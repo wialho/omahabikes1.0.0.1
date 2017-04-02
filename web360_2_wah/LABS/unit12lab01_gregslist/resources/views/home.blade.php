1c822edff27598da0a02596e3682b467a48abe00
waholland <waholland@mail.mccneb.edu>
2017-04-02 18:39:05 -0500

    unit12lab02_gregslist

---
+++ web360_2_wah/LABS/unit12lab01_gregslist/resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+    <head>
+    <title>gregsList - Omaha</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="{{asset('js/script.js')}}"></script>
+
+
+</head>
+<body style="display: none;">
+    <div id="left">
+        <h1>gregsList</h1>
+        <p><a href="#">post to classifieds</a> </p>
+        <p><a href="#">my account</a></p>
+        <p>search gregsList</p>
+        <form id="search" method="post" action="todo">
+            <label for="search-input">search</label><br>
+            <input type="text" name="search-input" /><br>
+            <input type="submit" />
+        </form>
+    </div>
+    <div id="right">
+        <h2>Omaha</h2>
+        <div id="content">
+            <h3> community</h3>
+            <ul>
+                <li>activities</li>
+                <li>classes</li>
+                <li>events</li>
+            </ul>
+            <h3> housing</h3>
+            <ul>
+                <li>apartments</li>
+                <li>rooms wanted</li>
+                <li>vacation rentals</li>
+            </ul>
+            <h3> community</h3>
+            <ul>
+                <li>admin</li>
+                <li>education</li>
+                <li>information technology</li>
+            </ul>
+        </div>
+    </div>
+    <footer>
+        <p>(C) Copyright 2017, GregsList</p>
+    </footer>
+</body>
+</html>
+
